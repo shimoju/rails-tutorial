86ccffed9d2761f892aca237da26c4265c3f7657
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-06-27 11:52:01 +0900

    有効なサインアップのテストを追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,4 +14,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # サインアップが失敗したとき、再度`users/new`がレンダリングされることをテスト
     assert_template 'users/new'
   end
+
+  test "valid signup information" do
+    get signup_path
+    # ブロックの実行前と後で、`User.count`の値が+1違うこと
+    assert_difference 'User.count', 1 do
+      # `post_via_redirect`：postしたあとリダイレクトを追う
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    # リダイレクトしたら`users/show`＝プロフィールページがレンダリングされること
+    assert_template 'users/show'
+  end
 end
