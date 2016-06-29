e61707c720cb824d8f7299ca1fe68d1a73102c56
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-06-29 12:20:03 +0900

    正しい情報でログインしたときのテストを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   # メールアドレス/パスワードが正しくないとき
   test "login with invalid information" do
     get login_path
@@ -12,5 +16,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     # 別のページにアクセスするとエラーメッセージが消えていること
     get root_path
     assert flash.empty?
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    # ログインできたらプロフィールページにリダイレクトすること
+    assert_redirected_to @user
+    # リダイレクトを追う（実際にリダイレクト先ページにアクセスする）
+    follow_redirect!
+    assert_template 'users/show'
+    # ログインページヘのリンクが存在しない(リンクが0個)こと
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
