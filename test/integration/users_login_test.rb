44b136cbaa20044d0e617a85941fb2740b22fdcf
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-06-29 14:43:42 +0900

    ログアウト処理を実装

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,9 +18,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
+    assert is_logged_in?
     # ログインできたらプロフィールページにリダイレクトすること
     assert_redirected_to @user
     # リダイレクトを追う（実際にリダイレクト先ページにアクセスする）
@@ -30,5 +31,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+
+    # logout_pathにDELETEリクエスト→destroyアクション
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
