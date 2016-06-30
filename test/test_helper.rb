e7a0bd9cf6b010c922b6b8ea4929e063ca659fc8
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-06-30 14:42:37 +0900

    `log_in_as `テストヘルパーを追加

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,4 +14,28 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # Logs in a test user.
+  def log_in_as(user, options = {})
+    # ハッシュはキーが存在しなければnilが返るので、`|| 'デフォルト値'`と書ける
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    # インテグレーションテストの場合は（通常のように）postすることでログインし、
+    # そうでない場合はsessionメソッドを直接使ってログインとする
+    if integration_test?
+      post login_path, session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+    # Returns true inside an integration test.
+    # `post_via_redirect`メソッドが定義されていればインテグレーションテストとする
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
