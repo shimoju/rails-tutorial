e4e4ef0daf02210d53c17c5d29f8dc9bf999bc53
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-06-30 17:42:08 +0900

    Ex1: クラスメソッド定義を`self.`を使う方法に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
   # Returns the hash digest of the given string.
-  def User.digest(string)
+  def self.digest(string)
     # 環境に応じてハッシュ値の計算コストを変更する
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   end
 
   # Returns a random token.
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
