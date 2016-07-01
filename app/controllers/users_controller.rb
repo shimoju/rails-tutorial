a09ab6ef92d098f253ec492a6238cfe9ac9b7142
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-01 15:08:26 +0900

    ログイン前にアクセスしようとしていたURLにリダイレクト(Friendly forwarding)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,8 @@ class UsersController < ApplicationController
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
+        # アクセスしようとしていたURLを保存し、ログインページにリダイレクト
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -47,4 +47,17 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # Redirects to stored location (or to the default).
+  # セッションに保存されているForwarding URLにリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  # アクセスしようとしていたURLをセッションに保存
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
