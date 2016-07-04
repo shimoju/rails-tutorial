ad0e25fdf39f9ecf072d2496cd895bbf7f9981c8
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-04 15:11:42 +0900

    Ex2: ログイン・ログアウトで変わるレイアウトリンクのテストを追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -8,5 +12,31 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+  end
+
+  # ログイン・ログアウトで変わるリンクのテスト
+  # ログインしていないとき
+  test "layout links for non-logged-in users" do
+    get root_path
+    # 表示するもの
+    assert_select "a[href=?]", login_path
+    # 表示しないもの
+    assert_select "a[href=?]", users_path, count: 0 # ユーザー一覧
+    assert_select "a[href=?]", user_path(@user), count: 0 # プロフィール
+    assert_select "a[href=?]", edit_user_path(@user), count: 0 # 設定
+    assert_select "a[href=?]", logout_path, count: 0 # ログアウト
+  end
+
+  # ログインしているとき
+  test "layout links for logged-in users" do
+    log_in_as(@user)
+    get root_path
+    # 表示するもの
+    assert_select "a[href=?]", users_path # ユーザー一覧
+    assert_select "a[href=?]", user_path(@user) # プロフィール
+    assert_select "a[href=?]", edit_user_path(@user) # 設定
+    assert_select "a[href=?]", logout_path # ログアウト
+    # 表示しないもの
+    assert_select "a[href=?]", login_path, count: 0
   end
 end
