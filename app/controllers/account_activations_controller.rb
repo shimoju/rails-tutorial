24b4bf014afaa9a900b250bde6e8926e83d22410
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-06 16:19:47 +0900

    アカウントのアクティベート処理を実装

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,18 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    # ユーザーが存在し、アクティベートされておらず、トークンが正しければ
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      # アクティベート処理を実行
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      # ログインしてリダイレクト
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
 end
