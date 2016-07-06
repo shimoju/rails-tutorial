ccb172efc9c2eaace05527d3bd747c71b967a90e
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-06 14:43:26 +0900

    アクティベーションメールのテストを追加

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "account_activation" do
-    mail = UserMailer.account_activation
+    user = users(:michael)
+    user.activation_token = User.new_token
+    # アクティベーションメールを作成
+    mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    # to, fromは配列
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.name,               mail.body.encoded
+    assert_match user.activation_token,   mail.body.encoded
+    # 本文記載のアクティベーションURLにあるメールアドレス(Query stringの値)
+    # を検索したいので、`CGI::escape`でエスケープする
+    # ex) http://example.com/account_activations/nn-N5qR0nRj7hAS4N5_bGQ/edit?email=michael%40example.com
+    assert_match CGI::escape(user.email), mail.body.encoded
   end
-
-  test "password_reset" do
-    mail = UserMailer.password_reset
-    assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
-
 end
