f6696cc8b1baba785488abe97785f54803cf41de
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-06 16:48:47 +0900

    アクティベーションのインテグレーションテストを追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    # deliveries(送信したメールの配列)はグローバルなので、毎回クリアする
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     # ブロックの実行前と後で、`User.count`の値が変わらないことをテストする
@@ -15,15 +21,37 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     # ブロックの実行前と後で、`User.count`の値が+1違うこと
     assert_difference 'User.count', 1 do
-      # `post_via_redirect`：postしたあとリダイレクトを追う
-      post_via_redirect users_path, user: { name:  "Example User",
-                                            email: "user@example.com",
-                                            password:              "password",
-                                            password_confirmation: "password" }
+      post users_path, user: { name:  "Example User",
+                               email: "user@example.com",
+                               password:              "password",
+                               password_confirmation: "password" }
     end
+    # メールが送信されていること
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    # Try to log in before activation.
+    # アクティベートする前はログインできないこと
+    log_in_as(user)
+    assert_not is_logged_in?
+    # Invalid activation token
+    # 不正なトークンを渡してもアクティベートできないこと
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    # Valid token, wrong email
+    # トークンは正しいがメールアドレスが間違っていたときもアクティベートできないこと
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # Valid activation token
+    # 正しいトークン・メールアドレスであればアクティベートしてログインできること
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
