a7f130ba692dc3291cc0f35ceb75f7d17b94a462
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-07 12:27:28 +0900

    パスワードリセットメールのテストを追加

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -17,4 +17,17 @@ class UserMailerTest < ActionMailer::TestCase
     # ex) http://example.com/account_activations/nn-N5qR0nRj7hAS4N5_bGQ/edit?email=michael%40example.com
     assert_match CGI::escape(user.email), mail.body.encoded
   end
+
+  test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Password reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.reset_token,        mail.body.encoded
+    # 上のアクティベーションメールと同様の理由でエスケープする
+    # ex) http://example.com/password_resets/xOaoHL9BRnfAq3Fw-Gs8lw/edit?email=michael%40example.com
+    assert_match CGI::escape(user.email), mail.body.encoded
+  end
 end
