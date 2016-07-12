f8ac3128abab05128a8bbbb44fd879abe3206607
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-12 17:58:24 +0900

    microposts#createアクションを実装

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,8 +2,23 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    # 現在のユーザーに関連付けられたmicropostを作成
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      # 投稿フォームはホームページ(root)に設置する
+      render 'static_pages/home'
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
