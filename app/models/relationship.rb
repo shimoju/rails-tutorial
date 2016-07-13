8329a7659a792be9f320584ce63e24f8abedad9d
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-13 17:54:33 +0900

    Relationshipモデルのバリデーションを設定

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,6 @@ class Relationship < ActiveRecord::Base
   # 外部キーは`user_id`なので、ここでは指定する必要はない
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
