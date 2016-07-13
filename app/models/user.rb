2613384c1a39a7c6a6cfa8d19dcededdda88425e
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-13 18:42:57 +0900

    has_many throughアソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  # `user.following.find(other_user)`などと扱えるようになる
+  has_many :following, through: :active_relationships, source: :followed
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
