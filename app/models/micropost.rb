5dd8ffdc303eedbecda54dd8aff192be85e9be56
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-13 12:33:48 +0900

    Micropostモデルにpicture属性を追加して、CarrierWaveと連携

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   # descending order：降順（新しい方から古い方へ）
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
