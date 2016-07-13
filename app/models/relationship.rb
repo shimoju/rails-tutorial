674c9e1716391836b726d6282cd986aa57f6c1d6
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-13 17:49:56 +0900

    UserモデルとRelationshipモデルでアソシエーションを設定

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,6 @@
 class Relationship < ActiveRecord::Base
+  # `foreign_key`はデフォルトでは`<クラス名(小文字)>_id`
+  # 外部キーは`user_id`なので、ここでは指定する必要はない
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
 end
