42a5a3428b1703f6593983491bf18979ccd8d459
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-07-14 17:38:28 +0900

    フォロー・アンフォローのインテグレーションテストを追加（Ajaxも含む）

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class FollowingTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other = users(:archer)
     log_in_as(@user)
   end
 
@@ -24,6 +25,39 @@ class FollowingTest < ActionDispatch::IntegrationTest
     assert_match @user.followers.count.to_s, response.body
     @user.followers.each do |user|
       assert_select "a[href=?]", user_path(user)
+    end
+  end
+
+  test "should follow a user the standard way" do
+    # 通常のpostリクエストでフォローが増えること
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, followed_id: @other.id
+    end
+  end
+
+  test "should follow a user with Ajax" do
+    # Ajax経由のpostリクエストでフォローが増えること
+    # xhrメソッドでAjaxリクエスト(XmlHttpRequest)を送れる
+    assert_difference '@user.following.count', 1 do
+      xhr :post, relationships_path, followed_id: @other.id
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    # 通常のdeleteリクエストでフォローを外せること
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "should unfollow a user with Ajax" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    # Ajax経由のdeleteリクエストでフォローを外せること
+    assert_difference '@user.following.count', -1 do
+      xhr :delete, relationship_path(relationship)
     end
   end
 end
